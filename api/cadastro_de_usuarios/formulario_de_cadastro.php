192a4bdef28c675f8296aafe9c6ed64e52a76aaf
Lucas Batista de Oliveira <lucasboliveira951@gmail.com>
2025-12-17 16:28:32 -0300

    Update formulario_de_cadastro.php

--- api/cadastro_de_usuarios/formulario_de_cadastro.php
+++ api/cadastro_de_usuarios/formulario_de_cadastro.php
@@ -50,8 +50,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="shortcut icon" href="/Cinedestino-main/assets/Image/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="../assets/css/cadastro.css">
+    <link rel="shortcut icon" href="/../assets/Image/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="/../assets/css/cadastro.css">
     <title>Criar Conta</title>
 </head>
 
@@ -77,7 +77,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </div>
     </main>
 
-    <script src="../assets/JavaScript/cadastro.js"></script>
 
     <script>
         document.addEventListener('DOMContentLoaded', () => {
@@ -103,6 +102,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         });
     </script>
+    
+    <script src="/../assets/JavaScript/cadastro.js"></script>
 </body>
 
 
