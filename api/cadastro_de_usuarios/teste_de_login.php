ae84caf864c25f47430e6bc874acb737964e6f76
Lucas Batista de Oliveira <lucasboliveira951@gmail.com>
2025-12-18 16:48:46 -0300

    Update teste_de_login.php

--- api/cadastro_de_usuarios/teste_de_login.php
+++ api/cadastro_de_usuarios/teste_de_login.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 ini_set('session.cookie_path', '/');
-ini_set('session.cookie_secure', '1');
+ini_set('session.cookie_secure', isset($_SERVER['HTTPS']) ? '1' : '0');
 ini_set('session.cookie_samesite', 'Lax');
 
 session_start();
