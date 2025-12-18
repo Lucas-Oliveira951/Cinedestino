c2cae22bdcdbc4bc9080ff77f14a9537847997c9
Lucas Batista de Oliveira <lucasboliveira951@gmail.com>
2025-12-18 18:55:55 -0300

    Update config_sessao.php

--- api/cadastro_de_usuarios/config_sessao.php
+++ api/cadastro_de_usuarios/config_sessao.php
@@ -4,7 +4,10 @@ error_reporting(E_ALL);
 
 ini_set('session.cookie_path', '/');
 ini_set('session.cookie_samesite', 'Lax');
-ini_set('session.cookie_secure', isset($_SERVER['HTTPS']) ? '1' : '0');
+ini_set('session.cookie_secure', '1'); 
+
+ini_set('session.use_strict_mode', 1);
+ini_set('session.use_only_cookies', 1);
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
