c9f2c57c81d28627e7c2c12ffcde3a7f41c32882
Lucas Batista de Oliveira <lucasboliveira951@gmail.com>
2025-12-18 19:05:40 -0300

    Update config_sessao.php

--- api/cadastro_de_usuarios/config_sessao.php
+++ api/cadastro_de_usuarios/config_sessao.php
@@ -2,12 +2,16 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-ini_set('session.cookie_path', '/');
-ini_set('session.cookie_samesite', 'Lax');
-ini_set('session.cookie_secure', '1'); 
-
-ini_set('session.use_strict_mode', 1);
-ini_set('session.use_only_cookies', 1);
+/**
+ * CONFIGURAÇÃO OBRIGATÓRIA PARA VERCEL
+ */
+session_set_cookie_params([
+    'lifetime' => 0,
+    'path' => '/',
+    'secure' => true,
+    'httponly' => true,
+    'samesite' => 'Lax'
+]);
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
